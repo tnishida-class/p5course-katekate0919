fb60cd6b556238d5ccaaeb097112eead4b310fb5
katekate0919 <2060564h@stu.kobe-u.ac.jp>
2021-10-11 22:59:05 +0900

    Sakurazaka 1-1

--- section1-1/sketch.js
+++ section1-1/sketch.js
@@ -1,15 +1,22 @@
 function setup() {
-  createCanvas(200,200);
+  createCanvas(128,128);
 }
 
 function draw() {
-  background(0);
+  background(255);
+  stroke(255)
   strokeWeight(4);
-  fill(178, 100, 162);
-  triangle(12,120,116,120,116,10);
-  fill(94, 185, 84);
-  triangle(0,108,106,108,106,0);
+  fill(130, 0, 181);
+  triangle(10,120,116,120,116,10);
+  fill(255)
+  triangle(106,10,106,108,10,108)
+  stroke(245, 130, 235)
+  strokeWeight(1);
   fill(255);
+  triangle(2,106,104,106,104,2);
+  stroke(255)
+  strokeWeight(4);
+  fill(245, 130, 235);
   textSize(32);
   textFont("serif");
   text("46", 68, 100);
