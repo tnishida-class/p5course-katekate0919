05474086585f9ef9d15e865de29582e3754ae29c
katekate0919 <2060564h@stu.kobe-u.ac.jp>
2021-10-14 22:00:04 +0900

    Update2-q2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -6,6 +6,28 @@ function setup() {
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+      if([i + j] % 2 == 1){
+        fill(150);
+      }
+      else{fill(255);}
+      console.log(i, j);
+      rect(size*i, size*j ,size, size);
     }
   }
+  for(let i = 0; i < 8; i++){
+    for(let j = 0; j < 8; j++){
+      // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+      if(j < 3 && [i + j] % 2 == 1){
+        fill(255,0,0);
+      }
+      else if(j > 4 && [i + j] % 2 == 1){
+        fill(0);
+      }
+      else if(j > 2 && j < 5 && [i + j] % 2 == 1){
+        fill(150);
+      }
+      else{fill(255);}
+      ellipse(i*size+size/2, j*size+size/2, size*9/10);
+    }
+}
 }
