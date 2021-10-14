778a91ff747efed8ba8ca6fc0f3c3cc55a71f2cf
katekate0919 <2060564h@stu.kobe-u.ac.jp>
2021-10-14 21:16:16 +0900

    Update2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,13 @@
 // 小手調べ
 function setup(){
+  createCanvas(200,200);
   for(let i = 0; i < 10; i++){
     // BLANK[1]
+    if (i < 5) {
+      stroke(0,0,255);
+    }
+    else{stroke(255,0,0)};
+    noFill();
+    ellipse(50,50,i*10+10);
   }
 }
