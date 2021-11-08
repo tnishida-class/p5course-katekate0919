38c52edac394376a1da7cf013ee956c1cf15f479
katekate0919 <2060564h@stu.kobe-u.ac.jp>
2021-11-08 16:33:56 +0900

    Update4-q1
    edit more

--- section4-q1/sketch.js
+++ section4-q1/sketch.js
@@ -15,10 +15,15 @@ function setup(){
   for(let i = 0; i < n; i++){ line(0, height * i / n, width, height * i / n); }
 
   // ここからが本番
-  fill(0);
+  stroke(0);
   const dx = width / scores.length;
-  let px, py; // 線を引くために一つ前の点を覚えておく変数
+  let py = height - height * scores[i - 1] / 100; // 線を引くために一つ前の点を覚えておく変数
   for(let i = 0; i < scores.length; i++){
     // BLANK[1]
-  }
+    const dy = height - height * scores[i] / 100;
+    strokeWeight(8);
+    point(i * dx, dy);
+    strokeWeight(4);
+    line(i * dx, dy, (i - 1) * dx, py);
+   }
 }
