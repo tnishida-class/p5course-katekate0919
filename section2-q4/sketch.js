dcaef1e6b93703e14f8863ef18c07afd397ebf97
katekate0919 <2060564h@stu.kobe-u.ac.jp>
2021-11-27 22:46:22 +0900

    Update 2-q4
    change 11/27

--- section2-q4/sketch.js
+++ section2-q4/sketch.js
@@ -1,28 +1,93 @@
 // ギリシャ国旗
 function setup() {
-  const blue = color(0, 51, 160);
+  const blue = color(50, 50, 120);
+  const red = color(190, 20, 15);
   createCanvas(270, 180);
   noStroke();
   background(255);
 
-  let d = height / 9; // 縞1本の太さ
+  let d = height / 13; // 縞1本の太さ
 
-  for(let i = 0; i < 9; i++){
+  for(let i = 0; i < 13; i++){
     // BLANK[1] (hint: 縞の色を交互に変えるには2で割った余りを使おう)
     if(i % 2 == 0){
-      fill(blue)
+      fill(red)
     }
     else{fill(255)}
     rect(0, i * d, width, (i + 1) * d);
   }
 
   fill(blue);
-  let size = d * 5;
-  rect(0, 0, size, size);
+  rect(0, 0, d * 9, d * 7);
 
-  fill(255);
-  noStroke()
-  // BLANK[2] (hint: 白い十字を描くには rect を二つ描こう)
-  rect(0, d*2, d*5, d)
-  rect(d*2, 0, d, d*5)
+  star(10, 5, 3);
+  star(30, 5, 3);
+  star(50, 5, 3);
+  star(70, 5, 3);
+  star(90, 5, 3);
+  star(110, 5, 3);
+
+  star(20, 15, 3);
+  star(40, 15, 3);
+  star(60, 15, 3);
+  star(80, 15, 3);
+  star(100, 15, 3);
+
+  star(10, 25, 3);
+  star(30, 25, 3);
+  star(50, 25, 3);
+  star(70, 25, 3);
+  star(90, 25, 3);
+  star(110, 25, 3);
+
+  star(20, 35, 3);
+  star(40, 35, 3);
+  star(60, 35, 3);
+  star(80, 35, 3);
+  star(100, 35, 3);
+
+  star(10, 45, 3);
+  star(30, 45, 3);
+  star(50, 45, 3);
+  star(70, 45, 3);
+  star(90, 45, 3);
+  star(110, 45, 3);
+
+  star(20, 55, 3);
+  star(40, 55, 3);
+  star(60, 55, 3);
+  star(80, 55, 3);
+  star(100, 55, 3);
+
+  star(10, 65, 3);
+  star(30, 65, 3);
+  star(50, 65, 3);
+  star(70, 65, 3);
+  star(90, 65, 3);
+  star(110, 65, 3);
+
+  star(20, 75, 3);
+  star(40, 75, 3);
+  star(60, 75, 3);
+  star(80, 75, 3);
+  star(100, 75, 3);
+
+  star(10, 85, 3);
+  star(30, 85, 3);
+  star(50, 85, 3);
+  star(70, 85, 3);
+  star(90, 85, 3);
+  star(110, 85, 3);
+}
+function star(cx, cy, r){
+  beginShape();
+  for(var i = 0; i < 5; i++){
+    fill(255);
+    stroke(255);
+    let theta = TWO_PI * i * 2 / 5 - HALF_PI;
+    let x = cx + cos(theta) * r;
+    let y = cy + sin(theta) * r;
+    vertex(x,y);
+  }
+  endShape(CLOSE);
 }
